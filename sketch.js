af2b978a86d5454dd7447fbe310bfda38d01b729
SujalThakkar <75084232+SujalThakkar@users.noreply.github.com>
2021-03-05 09:16:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ var gameover;
 
 function preload(){
   
-  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  monkey_running = loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
   
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png");
@@ -26,7 +26,7 @@ function preload(){
  
 }
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(800,1400)
   monkey = createSprite(width/2 - 300,height - 40, 20,10);
   monkey.addAnimation("running",monkey_running);
   monkey.scale = 0.12;
